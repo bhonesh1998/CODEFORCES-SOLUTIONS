948477385a195fc00624f909c42c6f9737ab4a9c
BHONESH CHAWLA <rajachawla778@gmail.com>
2018-09-26 09:23:30 +0530

    Create B - Star.cpp

---
+++ B - Star.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pf printf
+#define sf scanf
+#define db double
+#define z long long int
+#define mod 1000000007
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+z i,j;
+
+
+int main()
+{	fast;
+	 z n; cin>>n;
+	 cout << 6*n*n-6*n +1;
+	
+}
