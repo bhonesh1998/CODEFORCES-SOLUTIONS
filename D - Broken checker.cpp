0837f6c06e3d6bbc39e02b01a8426bdec1f92d9d
BHONESH CHAWLA <rajachawla778@gmail.com>
2018-09-26 09:22:59 +0530

    Create 	D - Broken checker.cpp

---
+++ D - Broken checker.cpp
@@ -0,0 +1,20 @@
+By bhoneshchawla, contest: April Fools Day Contest, problem: (D) Broken checker, Accepted, #
+ #include<bits/stdc++.h>
+using namespace std;
+#define pf printf
+#define sf scanf
+#define db double
+#define z long long int
+#define mod 1000000007
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+z i,j;
+
+
+int main()
+{	fast;
+	 z n;
+	 cin>>n;
+	if(n==5) cout << 1 <<endl;
+	else cout << n%3+1 <<endl;
+	
+}
